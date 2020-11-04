a22393c97bb3b5ed504be0a38bdfd6c383a39e4f
KaanOrnek <kaan.ornek@ug.bilkent.edu.tr>
2020-11-04 11:25:21 +0300

    Part A-3 is completed.
    Locatable interface is implemented.

---
+++ Locatable.java
@@ -0,0 +1,10 @@
+/*
+ * CS102 Lab03-A
+ * Mehmet Kaan Örnek 21901637
+ * 04.11.2020
+ */
+public interface Locatable {
+   public void setLocation( int x, int y );
+   public int getX();
+   public int getY();
+}
